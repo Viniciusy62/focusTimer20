bb97a477ca08e3111033b38db6075cdddf8a3f93
Vinicius Andrade <viniciusy62@gmail.com>
2023-09-04 21:35:45 -0300

    created new project

---
+++ src/main.js
@@ -0,0 +1,8 @@
+// Funcionalidades esperadas dos botões:
+
+// - Play   : aciona o timer;
+// - Stop   : para o timer;
+// - +    : aumenta em mais 5 minutos o tempo do timer;
+// - -    : diminui em 5 minutos o tempo do timer.
+
+// Obs: fique à vontade para adicionar animações ou alterar as cores dos cards.
