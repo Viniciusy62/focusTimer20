da102e5077d35baa7230ef072f4f49a7aa284b7c
Vinicius Andrade <viniciusy62@gmail.com>
2023-09-05 22:53:44 -0300

    import toggleMode.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
+import "../toggleMode.js"
 // Funcionalidades esperadas dos botões:
 
-// - Play   : aciona o timer;
-// - Stop   : para o timer;
-// - +    : aumenta em mais 5 minutos o tempo do timer;
-// - -    : diminui em 5 minutos o tempo do timer.
+// Play   : aciona o timer;
+// Stop   : para o timer;
+// +    : aumenta em mais 5 minutos o tempo do timer;
+// -    : diminui em 5 minutos o tempo do timer.
 
 // Obs: fique à vontade para adicionar animações ou alterar as cores dos cards.
+
+/*
+DEFINIR:
+index
+state
+elements
+events
+actions
+timer
+sounds
+toggleMode
+*/ 
+
+//STATE
+
+// const state = {
+//   isRunning: false,
+//   isMuted: true,
+//   isDark: false,
+// }
+
